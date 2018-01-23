7590a6fb1e243d191f42a2d7c921c3fe503e04a8
rebita <darklune@naver.com>
2018-01-23 21:35:51 +0900

    edit index.js for express

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 var express = require('express');
-var app = express.createServer(express.logger());
+var app = express();
+var bodyParser = require('body-parser');
+
+app.listen(4000,function(){
+    console.log("localhost 4000 server start");
+});
+app.use(function(req,res,next){
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
 app.get('/', function (request, response) {
     response.send('Hello World!');
-});
-var port = process.env.PORT || 5000;
-app.listen(port, function () {
-    console.log("Listening on " + port);
 });
